48000c7abdcd5e80a184534385ae242e3f890854
ranker-1 <victorodom99@gmail.com>
2024-11-12 20:13:17 -0500

    path to index

--- webserver/server.py
+++ webserver/server.py
@@ -111,6 +111,7 @@ def teardown_request(exception):
 #
 @app.route('/')
 def index():
+
   """
   request is a special object that Flask provides to access web request information:
 
@@ -173,7 +174,7 @@ def index():
   # render_template looks in the templates/ folder for files.
   # for example, the below file reads template/index.html
   #
-  return render_template("index.html", **context)
+  return render_template("/index.html/", **context)
 
 #
 # This is an example of a different path.  You can see it at
