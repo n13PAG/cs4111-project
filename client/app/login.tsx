aecdf52173b31b65655178307355e5d16cb4a72d
n13PAG <74218503+n13PAG@users.noreply.github.com>
2024-12-29 14:51:41 -0500

    Switch frontend framework to Expo and React

---
+++ client/app/login.tsx
@@ -0,0 +1,26 @@
+import {View, Text, StyleSheet, Button } from 'react-native';
+import React from 'react';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { TextInput } from 'react-native-gesture-handler';
+
+export default function LoginScreen() {
+    const [email, onChangeEmail] = React.useState('example@email.com')
+    const [password, onPasswordChange] = React.useState('')
+    
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <TextInput 
+                    onChangeText={onChangeEmail}
+                    value={email}
+                />
+                <TextInput 
+                    secureTextEntry={true}
+                    onChangeText={onPasswordChange}
+                    value={password}
+                />
+            </SafeAreaView>
+        </SafeAreaProvider>
+
+    )
+}
