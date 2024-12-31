ba98ec693ee097b2405d3d54760a3dbc59dbe507
n13PAG <74218503+n13PAG@users.noreply.github.com>
2024-12-31 18:19:15 -0500

    Updated signup page using yup validation

--- client/app/signup.tsx
+++ client/app/signup.tsx
@@ -1,9 +1,9 @@
-import { Text, View, Button, Alert } from "react-native"
+import { Text, View, Button, Alert, TextInput } from "react-native"
 import { useForm, Controller } from "react-hook-form"
 import React from 'react';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
-import { TextInput } from 'react-native-gesture-handler';
-import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 import { Component } from "react";
 
 class HttpExample extends Component {
@@ -36,92 +36,108 @@ class HttpExample extends Component {
 }
 
 export default function SignupScreen() {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      email: "",
-      uni: "",
-      name: "",
-      is_student: false,
-    },
-  })
-  const onSubmit = (data:any) => console.log(data)
+    const emailRegex = /@columbia.edu$/;
+    const schema = yup
+    .object({
+        username: yup
+            .string()
+            .required('User name is required'),
+        email: yup
+            .string()
+            .email('Invalid email')
+            .required('Email is required')
+            .matches(emailRegex, "Must a Columbia issued email"),
+        password: yup
+            .string()
+            .required('No password provided.')
+            .min(8, 'Password must contain at least 8 characters.')
+    })
 
-  return (
-    <SafeAreaProvider>
-        <SafeAreaView>
+    const {
+        control,
+        handleSubmit,
+        formState: {errors},
+    } = useForm({
+        resolver: yupResolver(schema),
+        defaultValues: {
+            email: '',
+            username: '',
+            password: '',
+        }
+    })
+
+    const onPressSend = (formData:any) => {
+        console.log(formData.username)
+        console.log(formData.email)
+        console.log(formData.password)
+
+        fetch('http://127.0.0.1:8111/login_request', {
+            method: 'GET'
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson)
+            this.setState({
+                data: responseJson
+            })
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    };
+
+    return (
+        <View>
+            <Text>Create an account</Text>
             <Controller
                 control={control}
                 rules={{
-                required: true,
+                    required: true,
                 }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                <TextInput
-                    placeholder="example@email.com"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                />
+                render={({field: {onChange, value}}) => (
+                    <TextInput 
+                        value={value}
+                        onChange={onChange}
+                        placeholder="Username"
+                    />
+                )}
+                name="username"
+            />
+            <Controller
+                control={control}
+                rules={{
+                    required: true,
+                }}
+                render={({field: {onChange, value}}) => (
+                    <TextInput 
+                        value={value}
+                        onChange={onChange}
+                        placeholder="Email"
+                    />
                 )}
                 name="email"
             />
-            {/* {errors.email && <Text>This is required.</Text>} */}
-
+            {errors.email && <Text>{errors.email.message}</Text>}
             <Controller
                 control={control}
                 rules={{
-                maxLength: 6,
-                required: true,
+                    required: true,
                 }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                <TextInput
-                    placeholder="UNI"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                />
+                render={({field: {onChange, value}}) => (
+                    <TextInput 
+                        value={value}
+                        onChange={onChange}
+                        placeholder="Password"
+                    />
                 )}
-                name="uni"
+                name="password"
             />
-            {/* {errors.uni[0] && errors.uni[1]} */}
-
-
-            <Controller
-                control={control}
-                rules={{
-                required: true,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                <TextInput
-                    placeholder="Name"
-                    onBlur={onBlur}
-                    onChangeText={onChange}
-                    value={value}
-                />
-                )}
-                name="name"
+            <Button 
+                title="Submit"
+                onPress={handleSubmit(onPressSend)} 
             />
-            {/* {errors.name && <Text>This is required.</Text>} */}
-
-            {/* <Controller
-                control={control}
-                rules={{
-                required: true,
-                }}
-                render={({ field: { onChange, onBlur, value } }) => (
-                    <BouncyCheckbox onPress={(value: boolean) => {= true}} />
-                )}
-                name="is_student"
-            /> */}
 
             <HttpExample></HttpExample>
-
-            <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-        </SafeAreaView>
-    </SafeAreaProvider>
-    
-  )
+        </View>
+    )
 }
